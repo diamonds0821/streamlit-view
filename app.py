0721a54e31caad0047cbe6b8b84b6146a7b35e10
diamonds0821 <122337424+diamonds0821@users.noreply.github.com>
2023-01-10 23:12:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.write('# 미다스하우징 데이터')
 st.write('#### 기간: 2023.01.01 - 2023.01.09 ')
 
 
-df = pd.read_csv('c:/data/230110.csv',thousands = ',')
-st.info('원본')
-st.write(df)
+#df = pd.read_csv('c:/data/230110.csv',thousands = ',')
+#st.info('원본')
+#st.write(df)
 #df1 = df.replace(['현매', 'DC'],['매출','반품'])
 #df2 = df1.drop(615)
 #df3 = df2.astype({'수량':int})
