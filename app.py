c401c94701d0de8ec32b01427df2f2a8073c5b3f
diamonds0821 <122337424+diamonds0821@users.noreply.github.com>
2023-01-10 22:03:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-view = [100, 150, 30]
-st.write('# youtube view')
-st.write('## raw')
-view
-st.write('## bar chart')
-st.bar_chart(view)
+
+st.write('# 미다스하우징 데이터')
+st.write('#### 기간: 2023.01.01 - 2023.01.09 ')
+
 import pandas as pd
-sview = pd.Series(view)
-sview
+df = pd.read_csv('c:/data/230110.csv',thousands = ',')
+df1 = df.replace(['현매', 'DC'],['매출','반품'])
+df2 = df1.drop(615)
+df3 = df2.astype({'수량':int})
+df4 = pd.pivot_table(df3, index='전표일자', columns='담당자', values=['총금액'], aggfunc='sum')
+df4
