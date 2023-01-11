479edaedc586b8087fe291f9aea5cd00d62c8a86
diamonds0821 <122337424+diamonds0821@users.noreply.github.com>
2023-01-11 15:48:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.write('## 미다스하우징 데이터')
-st.write('# 기간: 23.01.01 - 23.01.10')
+def main():
+   df = pd.read_csv('c:/data/230110.csv')
+   st.dataframe(df)
+   st.dataframe(df.style.highlight_max(axis=0))
+   st.table(df)
 
-df = pd.read_csv('c:/data/230110.csv',thousands = ',')
+   st.table(df.head())
+   st.write(df.head())
+   st.text(df.head())
 
-df1 = df.replace(['현매', 'DC'],['매출','반품'])
-df2 = df1.drop(615)
-df3 = pd.pivot_table(df2, index='전표일자', columns='구분', values=['총금액'], aggfunc='sum')
 
-st.dataframe(df3)
-#st.info('원본')
-
-#st.write(df)
-#df1 = df.replace(['현매', 'DC'],['매출','반품'])
-#df2 = df1.drop(615)
-#df3 = df2.astype({'수량':int})
-#df4 = pd.pivot_table(df3, index='전표일자', columns='담당자', values=['총금액'], aggfunc='sum')
-#st.write(df4)
+if __name__ == '__main__':
+   main()
